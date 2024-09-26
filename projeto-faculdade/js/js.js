35fa57bc7f27b86c8e1811a3111b67568fa8e43f
Nahtanm <nahtanmcs0019@gmail.com>
2024-09-26 15:15:10 -0300

    Atualizações

--- projeto-faculdade/js/js.js
+++ projeto-faculdade/js/js.js
@@ -1,3 +1,4 @@
+// Slide
 // Elementos Destaques
 const image = document.querySelector("#imagem-jogo");
 const mudarImage = document.querySelectorAll("#imagem-mudar li");
@@ -16,3 +17,15 @@ mudarImage.forEach((btn) =>{
         image.setAttribute("src", `img/${id}.jpeg`)
     });
 });
+
+// Carrossel
+// Elementos 
+const listJogos = ["img/jogo-1.jpeg","img/jogo-2.jpeg","img/jogo-3.jpeg"];
+let bannerAtual = 0;
+
+function trocarJogo(){
+    bannerAtual = (bannerAtual + 1) % listJogos.length;
+    document.querySelector("#imagem-jogo").src = listJogos[bannerAtual];
+}
+setInterval(trocarJogo, 3000);
+
